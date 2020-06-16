3bdbac6b7e475ffdf15863fae2f5a43d4c1e699c
Jair Aguilar <apjair97@gmail.com>
2020-06-16 17:52:38 -0500

    A menu was implemented to interact with the objects methods

--- main.py
+++ main.py
@@ -5,17 +5,38 @@ class Character():
         self.gender = gender
         self.atack = atack
 
-    def greet(self):
+    def greeting(self):
         print('Hi my name is {} and Im a {} {}, nice to meet you'.format(self.name,self.gender,self.type_))
 
-    def atack(self):
+    def do_atack(self):
         print('the {} named {} atack with {} '.format(self.type_,self.name,self.atack))
 
 
 if __name__ == "__main__":
     print('>>>>> Welcome to the creation menu <<<<<')
-    option = input('to continue press anykey or write "No" then press enter to exit')
+    option = input('to continue press anykey or write "No" then press enter to exit   ')
 
-    if option == 'No':
+    if option == 'no':
         print('Thanks for use this software')
+    else:
+        newChar = Character(input('Write the character name:  '), input('Write the type the type of your character example warrior, elf, ranger, thief, etc  '), input('Write your character gender:   '),input('Name your character attack:  '))
+ 
+        while option != 'no':
+            print('this is the character menu what do you want that {} do?'.format(newChar.name))
+            print('')
+            print('1:  Greet')
+            print('2:  Atack')
+            print('3:  Exit')
+            print('')
+
+            option2 = input('Select your option:   ')
+            print('')
+
+            if option2 == '1':
+                newChar.greeting()
+            if option2 == '2':
+                newChar.do_atack()
+            if option2 == '3':
+                option = 'no'
+
 
